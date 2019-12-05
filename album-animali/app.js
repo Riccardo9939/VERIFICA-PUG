74f4dab86c837042e2adece51f97c118ea53b728
Riccardo9939 <fabiovolo16@gmail.com>
2019-12-05 12:48:11 +0000

    Punto4

--- album-animali/app.js
+++ album-animali/app.js
@@ -1,12 +1,30 @@
 const express = require('express'); 
+const animali = require('./animali.json'); 
 const app = express();
 const path = require('path');
 var cors = require('cors'); 
+app.use(cors()); 
+
+app.set('view engine', 'pug');
 
 app.get('/api/album-animali', (req, res) => {
    res.sendFile(path.join(__dirname, 'animali.json'));
 });
 
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Animali',
+    animali: animali.animals
+  });
+});
+
+app.get('/album', (req, res) => {
+  const an = animali.animals.find(c => c.id_figurina === req.query.id_figurina);
+  res.render('album', {
+    title: `Animale: ${an.specie_animale}`,
+    an,
+  });
+});
 
 
 
